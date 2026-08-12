374ab30f4983d007c3bb4ee5da9681d8be6ad6c5
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-12 02:49:06 -0400

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,7 +17,6 @@ quick-sharun /usr/bin/DoomRunner
 #  /usr/lib/qt6/plugins/platformthemes/libqt6ct.so \
 #  /usr/lib/qt6/plugins/platformthemes/libqtlxqt.so \
 #  /usr/lib/qt6/plugins/styles/libkvantum.so
-#  /usr/lib/libpipewire-0.*.so
 
 # Additional changes can be done in between here
 
